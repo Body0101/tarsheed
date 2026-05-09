18039c465da0f8a7a0876797622dd43dadf2a499
body0101 <bsabr5198@gmail.com>
2026-05-09 22:13:03 +0300

    Updates

---
+++ .pio/build/esp32dev/FrameworkArduino/firmware_msc_fat.c.d
@@ -0,0 +1,3 @@
+.pio/build/esp32dev/FrameworkArduino/firmware_msc_fat.c.o: \
+ C:/Users/abd\ elrahman/.platformio/packages/framework-arduinoespressif32/cores/esp32/firmware_msc_fat.c \
+ C:/Users/abd\ elrahman/.platformio/packages/framework-arduinoespressif32/cores/esp32/firmware_msc_fat.h
---
+++ .pio/build/esp32dev/FrameworkArduino/libb64/cdecode.c.d
@@ -0,0 +1,3 @@
+.pio/build/esp32dev/FrameworkArduino/libb64/cdecode.c.o: \
+ C:/Users/abd\ elrahman/.platformio/packages/framework-arduinoespressif32/cores/esp32/libb64/cdecode.c \
+ C:/Users/abd\ elrahman/.platformio/packages/framework-arduinoespressif32/cores/esp32/libb64/cdecode.h
