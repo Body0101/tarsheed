6bb7c94769c7d94ef6a1ce16db0a721149f33bd5
body0101 <bsabr5198@gmail.com>
2026-05-11 19:08:24 +0300

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/tarsheed/sw.js', { scope: '/tarsheed/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
